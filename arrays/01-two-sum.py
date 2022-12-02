a25ad7de8e66c3aca3b5ecafb3ed5945474cf532
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2022-12-02 15:53:01 +0000

    add 01-two-sum

--- arrays/01-two-sum.py
+++ arrays/01-two-sum.py
@@ -3,23 +3,12 @@ from typing import List
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        solution = []
-        for num in nums:
-            new_nums = nums.copy()
-            new_nums.remove(num)
-            if target - num in new_nums:
-                solution.append(nums.index(num))
-                solution.append(new_nums.index(target - num) + 1)
-                break
-        return solution
-
-    def twoSum2(self, nums: List[int], target: int) -> List[int]:
-        [nums.remove(num) for num in nums if num > target]
-        for num in nums:
-            new_nums = nums.copy()
-            new_nums.remove(num)
-            if target - num in new_nums:
-                return [nums.index(num), new_nums.index(target - num) + 1]
+        seen = {}
+        for i, value in enumerate(nums):
+            remaining = target - nums[i]
+            if remaining in seen:
+                return [seen[remaining], i]
+            seen[value] = i
 
 
 if __name__ == "__main__":
@@ -27,19 +16,19 @@ if __name__ == "__main__":
     target = 9
     solution = Solution()
     assert solution.twoSum(nums, target) == [0, 1]
-    assert solution.twoSum2(nums, target) == [0, 1]
 
     nums2 = [3, 2, 4]
     target2 = 6
     assert solution.twoSum(nums2, target2) == [1, 2]
-    assert solution.twoSum2(nums2, target2) == [1, 2]
 
     nums3 = [3, 3]
     target3 = 6
     assert solution.twoSum(nums3, target3) == [0, 1]
-    assert solution.twoSum2(nums3, target3) == [0, 1]
 
     nums4 = [3, 2, 3]
     target4 = 6
     assert solution.twoSum(nums4, target4) == [0, 2]
-    assert solution.twoSum2(nums4, target4) == [0, 2]
+
+    nums5 = [0, 4, 3, 0]
+    target5 = 0
+    assert solution.twoSum(nums5, target5) == [0, 3]
