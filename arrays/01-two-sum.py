5b5d130994d1fd6452095d467a7cf8eb8d366842
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2022-12-03 15:34:28 +0000

    add 01-two-sum

--- arrays/01-two-sum.py
+++ arrays/01-two-sum.py
@@ -3,12 +3,12 @@ from typing import List
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        seen = {}
-        for i, value in enumerate(nums):
-            remaining = target - value
-            if remaining in seen:
-                return [seen[remaining], i]
-            seen[value] = i
+        numsMap = {}
+        for i, num in enumerate(nums):
+            remaining = target - num
+            if remaining in numsMap:
+                return [numsMap[remaining], i]
+            numsMap[num] = i
 
 
 if __name__ == "__main__":
